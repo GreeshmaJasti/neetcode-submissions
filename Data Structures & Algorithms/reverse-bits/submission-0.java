9ff56cd2a321e984467207720a3916dc5beb7901
Greeshma Jasti <103878190+GreeshmaJasti@users.noreply.github.com>
2026-07-08 14:57:06 -0400

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++){
+            int currentBit = (n >> i) & 1;
+            res += currentBit << (31 - i);
+        }
+        return res;
+    }
+}
